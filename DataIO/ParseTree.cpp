2938dca02e87a498348d388dda0b26bca4ed90c9
kzawisto <krystian.zawistowski4@gmail.com>
2015-12-04 09:25:11 +0100

    Intro

--- DataIO/ParseTree.cpp
+++ DataIO/ParseTree.cpp
@@ -25,80 +25,50 @@
 
 using namespace boost::property_tree;
 using namespace std;
-struct ENTRY{
- float val[5];
-} ;
-
-template<typename MyData>
-class BinParser{
-	string fileName,treeName,branch;
-	int leafnum;
-	int binnum;
-	MyData * arr;
-	 TTree* tree;
-	 TFile * file;
-	vector<TH1*> histos;
-	BinParser(string fileName, string treeName, string branch, int binnum, vector<TH1*> histos )
-	: fileName(fileName), treeName(treeName), branch(branch), leafnum(histos.size()), binnum(binnum), histos(histos){
-		arr = new MyData[leafnum];
-		file = new TFile("TestData.root");
-	}
-	void parse(ostream & out, string separator="\n"){
-		long entries = tree->GetEntries();
-		for(long i =0;i<entries;++i){
-			tree->GetEntry(i);
-			long result = 0;
-			for( unsigned j = 0; j < histos.size();++j){
-				histos[j]->FindBin(arr[j]);
-
-			}
-		}
-	}
-	~BinParser(){
-		delete [] arr;
-		delete [] file;
-		delete [] tree;
-	}
-
-};
 int main(){
-//	HistogramConfig hc("myproperties.json");
-//			for(auto & c : hc.vec){
-//				cout<<c.name<<" ";
-//			}
-//	TFile * file = new TFile("TestData.root");
-//		 TTree* tree = (TTree*)file->Get("TestTree");
-//		 fstream file1("multiplexed.bin", fstream::out | fstream::binary);
-//		 tree->GetEntries();
-//		 ENTRY aEntry;
-//		 //tree->Branch("entry",&aEntry,"mass/f:ptL1/f:ptL2/f:isoL1/f:isoL2/f");
-//		 float val[32];
-//		 for(int i =0;i<32;++i) val[i]=0;
-//		 tree->SetBranchAddress("entry", val);
-//		cout<<tree->GetEntries()<<"entries\n";
-//		int basis = 128;
-//		for(int i =0;i<100;++i){
-//			tree->GetEntry(i);
-//			for(int j=0;j<6;++j)cout<<val[j]<<" ";
-//			cout<<"\n";
-//			for(int k=0;k< hc.vec.size();++k){
-//							unsigned int j =hc.getBin(k,val[k]);
-//							cout<<j<<" ";
-//							file1.write((char*)&j, hc.vec[k].bytes);
-//
-//			}
-//			cout<<"\n\n";
-//
-//		}
-//		file1.close();
-//
-//		cout<<"\n"<<sizeof(short unsigned int);
 	ptree pt;
-	read_json("myproperties.json",pt);
+	read_json("config.json",pt);
 	for(auto & p : pt){
 
+		cout<<"\n"<<p.first;
 		HistoCreator data(p.second);
 		data.runTests();
-		cout<<"\n"<<p.first;
 	}
 }
+//struct ENTRY{
+// float val[5];
+//} ;
+
+//template<typename MyData>
+//class BinParser{
+//	string fileName,treeName,branch;
+//	int leafnum;
+//	int binnum;
+//	MyData * arr;
+//	 TTree* tree;
+//	 TFile * file;
+//	vector<TH1*> histos;
+//	BinParser(string fileName, string treeName, string branch, int binnum, vector<TH1*> histos )
+//	: fileName(fileName), treeName(treeName), branch(branch), leafnum(histos.size()), binnum(binnum), histos(histos){
+//		arr = new MyData[leafnum];
+//		file = new TFile("TestData.root");
+//	}
+//	void parse(ostream & out, string separator="\n"){
+//		long entries = tree->GetEntries();
+//		for(long i =0;i<entries;++i){
+//			tree->GetEntry(i);
+//			long result = 0;
+//			for( unsigned j = 0; j < histos.size();++j){
+//				histos[j]->FindBin(arr[j]);
+//
+//			}
+//		}
+//	}
+//	~BinParser(){
+//		delete [] arr;
+//		delete [] file;
+//		delete [] tree;
+//	}
+//
+//};
+
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # HistoGui
 A graphical tool for particle physics data analysis.
+
+Given series of events in ROOT data file, the program allows to plot histograms and apply 
+cutoffs to each histogram with wxWidgets based GUI. 
+
+Data are stored in multiplexed binary files,
+i.e. one needs to create such file from ROOT branch and JSON configuration file, employing provided command line tool. The GUI application uses the very same JSON config, when loading binary data.
+
+Example JSON configuration file and small ROOT file with junk data are provided in DataIO folder.JSON contains an arbitrary number of first level nodes (in the example there is one called "dat_file1"). 
+Below we have "option" node, comprising self-evident description of our files structure i.e. ROOT file, tree, and branch names, number of events to process and save in binary file,and binary file name. Second node at this level, "histos" contains the array of histogram descriptions, comprised of histogram name, the number of bins, and the range of values one can put in the histogram. In this case the range is redundant for now, since multiplexed values are equal to specific number of bins, but it makes the GUI application more universal. 
+
+To generate a binary datafile one needs to build ParseTree program, specifying path to the 
+ROOT config tool in DataIO/Makefile and running
+''' make ParseTree''' 
+in DataIO directory, provide config.json file and run the tool, typing just:
+'''./ParseTree'''.
+ 
+Then to run the gui one needs to build data access shared library and the GUI itself, typing 
+'''make'''
+in DataIO and HistogramGui dirs, and copy config.json and generated binary data file to HistogramGui directory.
+
+
